9490a08918760b9e103e982764c0051209666dce
wj <1354048405@qq.com>
2017-10-30 14:30:59 +0800

    侧边导航隐藏和退出登录完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -12,3 +12,27 @@ $(function(){
     },500);
   });
 });
+
+$(function(){
+  $('.lt_header span:first-child').on('click',function(){
+    $('.lt_side').toggleClass('side_hide');
+    $('.lt_header').toggleClass('big2');
+    $('.lt_main').toggleClass('big1');
+  });
+
+  $('.lt_header span:nth-child(2)').on('click',function(){
+   $('#lt_modal').modal('show');
+  });
+
+  $('.log_out').on('click',function(){
+    $.ajax({
+      url:'/employee/employeeLogout',
+      type:'get',
+      success:function(data){
+        if(data.success){
+          location.href = 'login.html';
+        }
+      }
+    })
+  })
+});
