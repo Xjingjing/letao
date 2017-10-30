a9e8be1a092d66daf02f357fda063db373eadb4d
wj <1354048405@qq.com>
2017-10-30 18:32:37 +0800

    用户禁用启用

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -9,6 +9,7 @@ $(function(){
 
   function render(){
 
+    //ajax请求数据
     $.ajax({
       url:'/user/queryUser',
       type:'get',
@@ -22,6 +23,34 @@ $(function(){
         var html = template('tpl',data);
         $('.lt_main tbody').html(html);
 
+
+        //禁用，启用功能
+        $('table button').on('click',function(){
+          var that = $(this);
+          var id = $(this).parent().data()['id'];
+          var isDelete = $(this).html() == "禁用"? 0:1;
+          $.ajax({
+            url:'/user/updateUser',
+            type:'post',
+            data:{
+              id:id,
+              isDelete:isDelete
+            },
+            success:function(data){
+              if(data.success){
+                if(isDelete == 0){
+                  that.html('启用').removeClass('btn-danger').addClass('btn-primary').parent().prev().html('禁用');
+
+                }else{
+                  that.html('禁用').removeClass('btn-primary').addClass('btn-danger').parent().prev().html('启用');
+
+                }
+              }
+            }
+           })
+        });
+
+        //分页配置
         $('#pagintor').bootstrapPaginator({
           numberOfPages:5,
           bootstrapMajorVersion:3,//默认是2，如果是bootstrap3版本，这个参数必填
